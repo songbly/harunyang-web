68d6c1c53fd56db44ff631fde893497652e8b3f1
“hyeseong” <hyeseong.ee@gmail.com>
2024-11-06 23:27:47 +0900

    에셋 및 헤더 추가

---
+++ src/app/assets/index.ts
@@ -0,0 +1,58 @@
+import logoWhite from "./harunyang-logo-white.svg";
+import logoBlack from "./harunyang-logo-black.svg";
+
+import catGreen from "./cat-green.svg";
+import catLetter from "./cat-letter.svg";
+import catRed from "./cat-red.svg";
+import catRedBgBlack from "./cat-red-black-bg.svg";
+import catSkyblue from "./cat-skyblue.svg";
+import catSkyblueBgBlack from "./cat-skyblue-black-bg.svg";
+import catYellow from "./cat-yellow.svg";
+import catYellowBgBlack from "./cat-yellow-black-bg.svg";
+
+import team1 from "./team-1.svg";
+import team2 from "./team-2.svg";
+import team3 from "./team-3.svg";
+import team4 from "./team-4.svg";
+import team5 from "./team-5.svg";
+import team6 from "./team-6.svg";
+import team7 from "./team-7.svg";
+import team8 from "./team-8.svg";
+
+import step1 from "./step-1@3x.png";
+import step2 from "./step-2@3x.png";
+import step3 from "./step-3@3x.png";
+import step4 from "./step-4@3x.png";
+
+import favicon from "./favicon-96x96.png";
+
+import appstore from "./appstore.svg";
+import playstore from "./playstore.svg";
+
+export {
+  logoWhite,
+  logoBlack,
+  catGreen,
+  catLetter,
+  catRed,
+  catRedBgBlack,
+  catSkyblue,
+  catSkyblueBgBlack,
+  catYellow,
+  catYellowBgBlack,
+  step1,
+  step2,
+  step3,
+  step4,
+  team1,
+  team2,
+  team3,
+  team4,
+  team5,
+  team6,
+  team7,
+  team8,
+  favicon,
+  appstore,
+  playstore,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,38 @@
+"use client";
+
 import Image from "next/image";
+import { catLetter, logoBlack, logoWhite } from "./assets";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <div
+        id="header"
+        className={"flex justify-center py-3 border-b border-gray-300"}
+      >
+        <div className="flex items-center justify-between w-[1440px]">
+          <Link href="/">
+            <Image src={logoBlack} width={160} height={56} alt="블랙 로고" />
+          </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex items-center justify-around w-[380px]">
+            <Link href={"#service"}>서비스</Link>
+            <Link href={"#team"}>팀</Link>
+            <Link href={"#ad"}>광고 문의</Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div id="service" className="h-dvh">
+        <p>고민이 많은 사람들을 위한\n 대화형 일기 서비스</p>
+        <p>가볍게 털어놓고 싶은 일상과 고민, 하루냥과 나눠보세요</p>
+        <button>
+            <Image src={appstore} width={1064} height={1064} alt="블랙 로고" />    
+        </button>
+        <Image src={catLetter} width={1064} height={1064} alt="블랙 로고" />
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
