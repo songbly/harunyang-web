db63bd6a6c265f602cdf97739d3e6a96a28a7980
“hyeseong” <hyeseong.ee@gmail.com>
2024-11-06 23:28:44 +0900

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { catLetter, logoBlack, logoWhite } from "./assets";
+import { appstore, catLetter, logoBlack, logoWhite } from "./assets";
 import Link from "next/link";
 
 const Home = () => {
